c89343fce7f7b043ce8e796d3e2c4880c98a793d
Abdullah Binladin <abdullahbinladin98@gmail.com>
2023-09-25 03:04:01 +0300

    fixed bugs with both implementations

--- binary-search.py
+++ binary-search.py
@@ -1,40 +1,41 @@
 def iterative_binary_search(left, right):
     iteration = 0
-    while right != left:
+    while right >= left:
         iteration += 1
         middle = (right + left) // 2
-        print(f"i:{iteration}: left: {left}\tright: {right}\t middle: {middle}")
+        print(f"i:{iteration}: left: {left}\t middle: {middle}\t right: {right}")
         if sorted_list[middle] > search_term:
             right = middle -1
         elif sorted_list[middle] < search_term:
             left = middle + 1
-        elif sorted_list[middle] == search_term:
+        else:
             print("located " + str(sorted_list[middle]) + " on iteration " +str(iteration))
-            break
-    print("item not in list")
+            return
+        
+    print(f"item {search_term} not in list")
 
 def recursive_binary_search(left, middle, right, iteration):
     iteration += 1
-    print(f"r:{iteration}: left: {left}\tright: {right}\t middle: {middle}")
+    print(f"r:{iteration}: left: {left}\t middle: {middle}\t right: {right}")
     if left == right:
-        print("item not in list")
+        print(f"term {search_term} not in list")
         return
     elif sorted_list[middle] > search_term:
         recursive_binary_search(left, (left + middle) // 2, middle - 1, iteration)
     elif sorted_list[middle] < search_term:
         recursive_binary_search(middle + 1, (middle + right) // 2, right, iteration)
-    elif sorted_list[middle] == search_term:
+    else:
         print(f"located {sorted_list[middle]} on iteration {iteration}")
         return
 
 
 
 if __name__ == "__main__":
-    sorted_list = [x + 1 for x in range(50)]
+    sorted_list = [x for x in range(50)]
     left = 0
     right = len(sorted_list) - 1
 
-    search_term = int(input("Input a number for me to guess!"))
+    search_term = int(input("Input a number for me to guess!: "))
     print("iterative binary search:")
     iterative_binary_search(left, right)
     print()
